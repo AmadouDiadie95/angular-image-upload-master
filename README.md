353498bff57cd86120fe5d549f05afc99f3a2615
Amadou Diadie Coulibaly <djimdemb45@gmail.com>
2022-03-26 15:28:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular-image-upload-master
+ 
---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+    title = 'Angular 13 File Upload';
+
+  constructor(private httpClient: HttpClient) { }
+
+  uploadedImage: File;
+  dbImage: any;
+  postResponse: any;
+  successResponse: string;
+  image: any;
+
+  public onImageUpload(event) {
+    this.uploadedImage = event.target.files[0];
+    console.log(this.uploadedImage)
+  }
+
+
+  imageUploadAction() {
+    const imageFormData = new FormData();
+    imageFormData.append('image', this.uploadedImage, this.uploadedImage.name);
+
+
+    this.httpClient.post('http://localhost:8080/upload/image/', imageFormData, { observe: 'response' })
+        .subscribe((response) => {
+              if (response.status === 200) {
+                this.postResponse = response;
+                this.successResponse = this.postResponse.body.message;
+              } else {
+                this.successResponse = 'Image not uploaded due to some error!';
+              }
+            }
+        );
+  }
+
+  viewImage() {
+    this.httpClient.get('http://localhost:8080/get/image/info/' + this.image)
+        .subscribe(
+            res => {
+              this.postResponse = res;
+              this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
+            }
+        );
+  }
+}
